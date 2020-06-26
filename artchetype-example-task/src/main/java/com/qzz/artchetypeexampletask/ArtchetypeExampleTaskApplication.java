44387c352c4df53f7f9297b1c62455e4f96f6d3a
2217736700@qq.com <2217736700@qq.com>
2020-06-26 21:36:51 +0800

    '初始化'

---
+++ artchetype-example-task/src/main/java/com/qzz/artchetypeexampletask/ArtchetypeExampleTaskApplication.java
@@ -0,0 +1,13 @@
+package com.qzz.artchetypeexampletask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtchetypeExampleTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtchetypeExampleTaskApplication.class, args);
+    }
+
+}
